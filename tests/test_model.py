a04bee3c7e430fbf64e5428594d512e91e6a58a6
HHoofs <huubhoofs@gmail.com>
2023-05-27 14:59:11 +0200

    changes

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,6 @@
+import random
+from itertools import chain
+from pprint import pprint
 from string import ascii_uppercase, ascii_lowercase
 
 from scheduler.model import SchedulePlaneMaintenance
@@ -85,3 +88,59 @@ def test_complex_plane():
     worker_schedule, deployable_planes = scheduler.optimize()
     assert len(deployable_planes) == 3
     assert 'Fokker' not in deployable_planes
+
+
+def test_2day_task():
+    planes = {
+        'F16': ('A', 'B', ),
+        'JSF': ('C', 'D', ),
+        'MiG': ('E', ),
+    }
+
+    workers = {
+        'Arthur': {q for q in ascii_lowercase},
+        'Roy': {q for q in ascii_lowercase},
+        'Brown': {q for q in ascii_lowercase},
+    }
+
+    tasks = {
+        'A': ({'a', 'b', 'c'}, 2),
+        'B': ({'a', 'b', 'c'}, 2),
+        'C': ({'a', 'b', 'c'}, 2),
+        'D': ({'a', 'b', 'c'}, 2),
+        'E': ({'a', 'b', 'c'}, 16),
+    }
+
+    scheduler = SchedulePlaneMaintenance(planes,
+                                         workers,
+                                         tasks)
+    _, deployable_planes = scheduler.optimize()
+    assert len(deployable_planes) == 2
+
+
+def test_large_test_set():
+    _random = random.Random(5)
+
+    planes = {
+        ''.join(_random.choices(ascii_lowercase, k=10)).title():
+            tuple((''.join(_random.choices(ascii_lowercase, k=3)).upper()
+                   for i in range(_random.randint(2,6))))
+        for _ in range(10)
+    }
+
+    workers = {
+        ''.join(_random.choices(ascii_lowercase, k=5)).title():
+            {t for t in ascii_lowercase if _random.random() > .1}
+        for _ in range(10)
+    }
+
+    tasks = {
+        task: ({t for t in ascii_lowercase if _random.random() > .9}, _random.randint(0,8))
+        for task in chain.from_iterable(planes.values())
+    }
+
+    scheduler = SchedulePlaneMaintenance(planes,
+                                         workers,
+                                         tasks)
+    _, deployable_planes = scheduler.optimize()
+    assert len(deployable_planes) == 7
---
+++ 🛦.py
@@ -0,0 +1,29 @@
+from pprint import pprint
+from string import ascii_lowercase
+
+from scheduler.model import SchedulePlaneMaintenance
+
+planes = {
+    'F16': ('F16-wings', 'F16-propellers',),
+    'JSF': ('JSF-propellers',),
+    'MiG': ('MiG-tail', 'MiG-cockpit',)
+}
+
+workers = {
+    'Arthur': {q for q in ascii_lowercase},
+    'Roy': {q for q in ascii_lowercase},
+    'Brown': {q for q in ascii_lowercase},
+}
+
+tasks = {
+    'F16-wings': ({'a', 'b', 'c'}, 2),
+    'F16-propellers': ({'a', 'b', 'c'}, 2),
+    'JSF-propellers': ({'a', 'b', 'c'}, 2),
+    'MiG-tail': ({'a', 'b', 'c'}, 2),
+    'MiG-cockpit': ({'a', 'b', 'c'}, 2),
+}
+
+scheduler = SchedulePlaneMaintenance(planes, workers, tasks)
+worker_schedule, deployable_planes = scheduler.optimize()
+pprint(worker_schedule)
+pprint(deployable_planes)
